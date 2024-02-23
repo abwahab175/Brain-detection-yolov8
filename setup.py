bfe5af0927b5fc621e0e78a523922e29e93eb3b6
“[abwahab175]” <[abwahab175@gmail.com]>
2024-02-24 00:26:52 +0500

    data added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'BrainDetection',
+    version= '0.0.0',
+    author= 'Abdul Wahab',
+    author_email= 'abwahab175@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
